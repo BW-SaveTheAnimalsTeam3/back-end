39e9d8e42c74ac3b7b687f474a3316e93b12bb0a
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-03 15:02:59 -0700

    added organizations table

--- data/migrations/20200203141521_users_organizations_campaigns_donations.js
+++ data/migrations/20200203141521_users_organizations_campaigns_donations.js
@@ -1,13 +1,24 @@
 
 exports.up = function(knex) {
-  return knex.schema.creatTable('users', tbl => {
+  return knex.schema.createTable('users', tbl => {
         tbl.increments('user_id');
         tbl.string('username')
             .notNullable()
             .unique();
         tbl.string('password')
             .notNullable()
-  })
+    })
+    .createTable('organizations', tbl => {
+        tbl.increments('org_id');
+        tbl.string('org_name')
+            .notNullable()
+            .unique();
+        tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('user_id')
+            .inTable('users');
+    })
 };
 
 exports.down = function(knex) {
