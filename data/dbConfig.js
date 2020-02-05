e8b81ddd258c1137c4000f9a1d9d383b54e5d8da
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-05 11:31:58 -0700

    fixed issue in dbConfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,13 +1,7 @@
+require('dotenv').config();
+
 const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const environment = process.env.DB_CONNECT || "development";
 
-const configOptions = require('../knexfile').development;
-
-module.exports = knex(configOptions);
-
-// require('dotenv').config();
-
-// const knex = require('knex');
-// const knexConfig = require('../knexfile.js');
-// const environment = process.env.DB_CONNECT || "development";
-
-// module.exports = knex(knexConfig[environment]);
+module.exports = knex(knexConfig[environment]);
