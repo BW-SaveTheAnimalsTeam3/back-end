db570e63d198486f805351e5d94b94085fd715ab
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-05 10:27:58 -0700

    fixed user.id issue in usersRouter

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -49,7 +49,7 @@ router.post('/login', (req, res) => {
             if (user && bcrypt.compareSync(password, user.password)){
                 const token = generateToken(user)
 
-                res.status(200).json({message: `Welcome ${user.username}!`, token, user_id: user.user_id });
+                res.status(200).json({message: `Welcome ${user.username}!`, token, id: user.id });
             } else {
                 res.status(401).json({message: 'Invalid Credentials'})
             }
