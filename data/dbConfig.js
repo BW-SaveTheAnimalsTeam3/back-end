2375a9eb556ca2cc65d35435983d6f29e6e036d7
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-05 11:28:42 -0700

    added env variable in dbConfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,3 +3,11 @@ const knex = require('knex');
 const configOptions = require('../knexfile').development;
 
 module.exports = knex(configOptions);
+
+// require('dotenv').config();
+
+// const knex = require('knex');
+// const knexConfig = require('../knexfile.js');
+// const environment = process.env.DB_CONNECT || "development";
+
+// module.exports = knex(knexConfig[environment]);
