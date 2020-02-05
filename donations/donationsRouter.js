8f3ece59e729130209b6e333cfd89ddfa3284f0f
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-05 02:01:57 -0700

    added post endpoint to donationsRouter

--- donations/donationsRouter.js
+++ donations/donationsRouter.js
@@ -4,6 +4,16 @@ const router = express.Router();
 
 const donations = require('./donationsModel.js');
 
+router.post('/', (req, res) => {
+    const details = req.body;
 
+    donations.add(details)
+        .then(donation => {
+            res.status(201).json(donation)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
 
 modules.export = router
