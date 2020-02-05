6d84cf07f1d28f88cb6561a81a341b8c4a8ab13a
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-04 23:43:56 -0700

    added jwt to login return

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -32,7 +32,9 @@ router.post('/login', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)){
-                res.status(200).json({message: `Welcome ${user.username}!`});
+                const token = generateToken(user)
+
+                res.status(200).json({message: `Welcome ${user.username}!`, token, user_id: user.user_id });
             } else {
                 res.status(401).json({message: 'Invalid Credentials'})
             }
@@ -41,6 +43,19 @@ router.post('/login', (req, res) => {
             res.status(500).json(error);
         });
 })
+
+function generateToken(user){
+    const payload = {
+        subject: user.user_id,
+        username: user.username,
+    };
+    const secret = 'Secret sauce';
+    const options = {
+        expiresIn: '1d'
+    }
+
+    return jwt.sign(payload, secret, options)
+}
 
 router.get('/', (req, res) => {
     users.findAll()
