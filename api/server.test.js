503a4d38fa7a6d96a644a964db81aeb7173b1d77
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-07 00:32:39 -0700

    added register route and login route tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,2 +1,32 @@
 const server = require('./server.js');
+const request = require('supertest');
 
+describe('GET /', () => {
+    it('returns 200 OK', () => {
+        return request(server).get('/')
+            .expect(200)
+    })
+})
+
+describe('register route', () => {
+    it('sends 500 status if no email and password', () => {
+        return request(server)
+            .post('/api/users/register')
+            .then(res => expect(res.status).toBe(500))
+    })
+
+    // it('sends status 201 upon success', () => {
+    //     return request(server)
+    //         .post('/api/users/register')
+    //         .send({ username: 'testTest', password: 'testTest'})
+    //         .then(res => expect(res.status).toBe(201))
+    // })
+})
+
+describe('login route', () => {
+    it('sends 500 status if login creds are unregistered', () => {
+        return request(server)
+            .post('/api/users/register')
+            .then(res => expect(res.status).toBe(500))
+    })
+})
